c0ae1eff8fb2543b73ae4fb64e24a373526cef38
Aaron Schuster <aaronjschuster@gmail.com>
2023-09-03 10:55:53 -0400

    display parks from DB on screen

---
+++ src/components/Park.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Park( { park } ) {
+    return (
+        <div>
+            {park.name}
+        </div>
+    );
+}
+
+export default Park;
--- src/components/ParkSelect.js
+++ src/components/ParkSelect.js
@@ -1,8 +1,14 @@
-function ParkSelect() {
+import React from "react";
+
+import Park from "./Park";
+
+function ParkSelect( {parks} ) {
     return (
         <div>
             <input placeholder="Search..."/>
-
+            {parks.map(park => {
+                return <Park park={park} key={park.parks_id}/>;
+            })}
         </div>
     )    
 }
