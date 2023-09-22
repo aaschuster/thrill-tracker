9e09457f4eeb33123ea141c3fb9e85735e4cf75a
Aaron Schuster <aaronjschuster@gmail.com>
2023-09-22 14:18:28 -0400

    set up create account form to be stored/updated in state

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import MainHeader from "./MainHeader";
@@ -8,29 +9,61 @@ import "../styles/CreateAccount.css";
 const serverURL = process.env.REACT_APP_SERVERURL;
 
 const CreateAccount = () => {
+
+    const navigate = useNavigate();
+
+    const initForm = {
+        email: "",
+        username: "",
+        password: "",
+        confirmpassword: ""
+    }
+
+    const [form, setForm] = useState(initForm);
+
+    function onChange(evt) {
+        const {target} = evt;
+        setForm({...form, [target.id]: target.value});
+    }
+
+    function onSubmit(evt) {
+        evt.preventDefault();
+        console.log(form);
+    }
+
     return (
         <div className="createaccount">
             <MainHeader/>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="formitem">
                     <label>Email:
                     </label>
-                    <input/>
+                    <input onChange={onChange} id="email" value={form.email}/>
                 </div>
                 <div className="formitem">
                     <label>Username:
                     </label>
-                    <input/>
+                    <input onChange={onChange} id="username" value={form.username}/>
                 </div>
                 <div className="formitem">
                     <label>Password:
                     </label>
-                    <input type="password"/>
+                    <input 
+                        type="password"
+                        onChange={onChange}
+                        id="password"
+                        value={form.password}
+                    />
                 </div>
                 <div className="formitem">
                     <label>Confirm password:
                     </label>
-                    <input type="password"/>
+                    <input
+                        type="password"
+                        onChange={onChange}
+                        id="confirmpassword"
+                        value={form.confirmpassword}
+                    />
                 </div>
                 <button>Submit</button>
             </form>
