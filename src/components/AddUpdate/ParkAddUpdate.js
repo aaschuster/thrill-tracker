e3861aeacf2f51c9d6eed039d139f29cea6731f5
Aaron Schuster <aaronjschuster@gmail.com>
2023-11-05 13:05:21 -0500

    replace dropdown menus for parkaddupdate with DatalistInput

--- src/components/AddUpdate/ParkAddUpdate.js
+++ src/components/AddUpdate/ParkAddUpdate.js
@@ -2,8 +2,9 @@ import React, { useState, useRef, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 
+import DatalistInput from 'react-datalist-input';
+
 import BackButton from "../BackButton";
-import Dropdown from "../Dropdown";
 
 import "../../styles/ParkAddUpdate.css";
 
@@ -20,39 +21,31 @@ function ParkAddUpdate( {chains, states, countries} ) {
     }
 
     const [form, setForm] = useState(initForm);
+    const [datalists, setDatalists] = useState({});
     const [err, setErr] = useState("");
-    const [inputWidth, setInputWidth] = useState(100);
-    const [openDropdown, setOpenDropdown] = useState("none");
-
-    const inputRef = useRef(null);
-
-    const labelSize = 125;
-
-    function searchList(list, keyword) {
-        keyword = keyword.toLowerCase();
-        return list.filter( li => {
-            if(li.name.toLowerCase().includes(keyword)) return li;
-        })
-    }
 
     function onChange(evt) {
         setErr("");
         const {target} = evt;
-        setOpenDropdown(target.id);
         setForm({...form, [target.id]: target.value})
     }
 
     useEffect(() => {
-
-        setInputWidth(inputRef.current.offsetWidth);
-
-    }, [inputWidth])
+        setDatalists({...datalists, 
+            chains: chains.map( chain => {
+                return{id: chain.chains_id, value: chain.name}
+            }),
+            states: states.map( state => {
+                return{id: state.states_id, value: state.name}
+            }),
+            countries: countries.map( country => {
+                return{id: country.countries_id, value: country.name}
+            }),
+        });
+    }, [])
 
     return (
         <div className="parkaddupdate">
-            {
-                openDropdown === "none" ? <></> : <div className="background" onClick={() => setOpenDropdown("none")}></div>
-            }
             <div className="parkaddupdateheader">
                 <BackButton/>
                 <h2>Add Park</h2>
@@ -60,47 +53,48 @@ function ParkAddUpdate( {chains, states, countries} ) {
             <form>
 
                 <div className="formitem">
-                    <label>Name:</label>
-                    <input id="name" value={form.name} onChange={onChange} ref={inputRef}/>
+                    <label className="inputlabel">Name:</label>
+                    <input id="name" value={form.name} onChange={onChange} autoFocus/>
                 </div>
 
                 <div className="formitem">
-                    <label>Chain:</label>
-                    <div>
-                        <input id="chain" value={form.chain} onChange={onChange}/>
-                        <div 
-                            className={`dropdowncontainer ${ openDropdown === "chain" ? "" : "hidden"}`} 
-                            style={{width: inputWidth-4}}
-                        >
-                            <Dropdown items={searchList(chains, form.chain)} />
-                        </div>
+                    <label className="inputlabel">Chain:</label>
+                    <div className="datalistcontainer">
+                        {
+                            datalists.chains ?
+                            <DatalistInput
+                                items={datalists.chains}
+                            /> : <></>
+                        }
                     </div>
                 </div>
 
                 <div className="formitem">
-                    <label>State:</label>
-                    <input id="state" value={form.state} onChange={onChange}/>
-                </div>
-
-                <div className={`dropdowncontainer ${ openDropdown === "state" ? "" : "hidden"}`} style={{
-                    width: inputWidth-4
-                }}>
-                    <Dropdown items={states} />
-                </div>
-
-                <div className="formitem">
-                    <label>Country:</label>
-                    <input id="country" value={form.country} onChange={onChange}/>
-                </div>
-
-                <div className={`dropdowncontainer ${ openDropdown === "country" ? "" : "hidden"}`} style={{
-                    width: inputWidth-4
-                }}>
-                    <Dropdown items={countries} />
+                    <label className="inputlabel">State:</label>
+                    <div className="datalistcontainer">
+                        {
+                            datalists.states ?
+                            <DatalistInput
+                                items={datalists.states}
+                            /> : <></>
+                        }
+                    </div>
                 </div>
 
                 <div className="formitem">
-                    <label>Opening year:</label>
+                    <label className="inputlabel">Country:</label>
+                    <div className="datalistcontainer">
+                        {
+                            datalists.countries ?
+                            <DatalistInput
+                                items={datalists.countries}
+                            /> : <></>
+                        }
+                    </div>
+                </div>
+
+                <div className="formitem">
+                    <label className="inputlabel">Opening year:</label>
                     <input id="openingyear" value={form.openingyear} onChange={onChange}/>
                 </div>
 
